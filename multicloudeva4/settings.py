981534cdbf594806c6d6a5b8e778d058661c7ed1
Baltica322 <pablocamposoyarzunccp@gmail.com>
2025-12-17 22:51:41 -0300

    xd

--- multicloudeva4/settings.py
+++ multicloudeva4/settings.py
@@ -58,6 +58,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
