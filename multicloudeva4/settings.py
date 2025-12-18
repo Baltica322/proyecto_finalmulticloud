b77411daea5212d774478c743c18c3742f1bee2f
Baltica322 <pablocamposoyarzunccp@gmail.com>
2025-12-17 22:48:22 -0300

    mas en settings

--- multicloudeva4/settings.py
+++ multicloudeva4/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,10 +52,18 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'multicloudeva4.urls'
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,"templates")
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
